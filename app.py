4ffad677ceb038b9eb27a0176f4b3a053fb9a47e
giorgiodemarchi <giorgio_demarchi@yahoo.com>
2024-04-14 17:39:08 -0400

    fix

--- app.py
+++ app.py
@@ -10,7 +10,13 @@ async def compress_prompt(input_prompt: str):
     """
     Calls LLMLingua PromptCompressor and returns the compressed prompt
     """
+
+    lines = input_prompt.splitlines()
+
+    context = "\n".join(lines[:-1])
+
+    question = lines[-1]
     
-    compressed_prompt = llm_lingua.compress_prompt(context, instruction="", question=question, target_token=200)
+    compressed_prompt = llm_lingua.compress_prompt(context, instruction="", question=question, target_token=500)
 
     return compressed_prompt['compressed_prompt']
